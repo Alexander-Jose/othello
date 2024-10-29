76830c2c83ef27a99d904003fc266787b3741be3
AVolkoff6 <135155743+Alexander-Jose@users.noreply.github.com>
2024-10-29 13:25:28 -0500

    Basic board display

---
+++ go.mod
@@ -0,0 +1,3 @@
+module othello
+
+go 1.23.1
---
+++ othello.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// Represents a possible configuration of Othello pieces
+// 0 represents a blank space
+// 1 represents a black piece
+// 2 represents a white piece
+//Todo: following states uncertain
+// 3 represents a possible black move
+// 4 represents a possible white move
+type boardstate = [8][8]byte
+
+// Returns an initial starting state for the board
+func initialState() boardstate {
+	var blankBoard boardstate
+
+	//Initializes the center four pieces in a checkerboard pattern
+	blankBoard[4][4] = 1
+	blankBoard[3][3] = 1
+	blankBoard[3][4] = 2
+	blankBoard[4][3] = 2
+
+	return blankBoard
+
+}
+
+func displayBoardState(state boardstate) {
+
+	// Values used to display pieces on a board. The characters correpond to the indexes used in the board state
+	displayCharacters := []rune{'□', '●', '○', '◌', '◌'}
+
+	//Todo: Add row labels
+	for rowIndex, row := range state {
+		for columnIndex, itemValue := range row {
+			fmt.Printf("%s ", string(displayCharacters[itemValue]))
+			// Todo: remove. If unused, ignore values
+			_ = columnIndex
+		}
+		fmt.Println()
+		// Todo: remove. If unused, ignore values
+		_ = rowIndex
+	}
+
+}
+
+func main() {
+	//Todo: When initializing, print rules
+	fmt.Println("")
+
+	board := initialState()
+	displayBoardState(board)
+
+}
